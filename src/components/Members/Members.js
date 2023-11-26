5b541b2aa91406429c12e1088de55286494f9749
kosar mirshahi <kosarmirshahi@gmail.com>
2023-11-26 19:10:28 +0330

    add users

---
+++ src/components/Members/Members.js
@@ -0,0 +1,20 @@
+import User from "../User/User";
+
+function Members() {
+  return (
+    <div class="container mx-auto py-6 w-9/12 mt-7">
+      <table class="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th class="border border-gray-300 px-4 py-2">Image</th>
+            <th class="border border-gray-300 px-4 py-2">Full name</th>
+            <th class="border border-gray-300 px-4 py-2">Email address</th>
+          </tr>
+        </thead>
+        <User />
+      </table>
+    </div>
+  );
+}
+
+export default Members;
---
+++ src/components/User/User.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+function User() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const response = await fetch("https://reqres.in/api/users?page=2");
+      const data = await response.json();
+      setData(data.data);
+    }
+    fetchUserData();
+  }, []);
+
+  //   console.log(data);
+  return (
+    <tbody>
+      {data.map((user) => (
+        <tr key={user.id}>
+          <td class="border border-gray-300 px-4 py-2">
+            <img
+              className="w-16 h-16 rounded-full"
+              src={user.avatar}
+              alt="pic"
+            />
+          </td>
+          <td class="border border-gray-300 px-4 py-2">
+            {user.first_name} {user.last_name}
+          </td>
+          <td class="border border-gray-300 px-4 py-2">{user.email}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+export default User;
